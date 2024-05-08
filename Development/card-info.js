4d8497c478cef7d6af426be370bbe63c2df150c0
Bedirhan <bedirhan_kurt_@outlook.com>
2024-05-08 10:09:27 +0200

    Upload Files

---
+++ Development/card-info.js
@@ -0,0 +1,6 @@
+const addIcon = document.getElementsByClassName('addIcon')[0]
+const cardInfo = document.getElementsByClassName('card-info')[0]
+
+addIcon.addEventListener('click', function(){
+    cardInfo.style.display = 'block'
+})
---
+++ Development/changebg.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.changebg-btn').addEventListener('click', function() {
+        document.getElementById('fileInput').click();
+    });
+});
+
+function changeBg(input) {
+    
+}
