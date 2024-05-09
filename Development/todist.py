08737e33b14eb1a90ec0450733116c812d142d22
Bedirhan <bedirhan_kurt_@outlook.com>
2024-05-09 20:04:01 +0200

    update

--- Development/todist.py
+++ Development/todist.py
@@ -1,4 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///veritabani.db'  # SQLite veritabanı dosyası yolu
+app.config['SECRET_KEY'] = 'gizli_anahtar'  # Flask-Admin için gizli anahtar
+db = SQLAlchemy(app)
+
+# Veritabanı modeli oluşturma
+class Tablo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    veri = db.Column(db.String(100))
+
+# Flask-Admin yönetici paneli oluşturma
+admin = Admin(app, name='Flask-Admin', template_mode='bootstrap3')
+admin.add_view(ModelView(Tablo, db.session))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 app = Flask(__name__)
 
