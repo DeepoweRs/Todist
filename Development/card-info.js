67c13c3d1348c1ad7b97058ce787b8fbbfd4b18d
Bedirhan <bedirhan_kurt_@outlook.com>
2024-05-09 19:30:52 +0200

    update

--- Development/card-info.js
+++ Development/card-info.js
@@ -1,6 +1,15 @@
 const addIcon = document.getElementsByClassName('addIcon')[0]
 const cardInfo = document.getElementsByClassName('card-info')[0]
+const confirm = document.getElementsByClassName('confirm-btn')[0]
+const cancel = document.getElementsByClassName('cancel-btn')[0]
 
 addIcon.addEventListener('click', function(){
     cardInfo.style.display = 'block'
+})
+
+confirm.addEventListener("click", function(){
+    cardInfo.style.display = "none"
+})
+cancel.addEventListener("click", function(){
+    cardInfo.style.display = "none"
 })
