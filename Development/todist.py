bafa0044f24c9f64ca0d172236060d7ec7beee03
Bedirhan <bedirhan_kurt_@outlook.com>
2024-05-13 08:31:53 +0200

    update

--- Development/todist.py
+++ Development/todist.py
@@ -2,27 +2,28 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-
-'''app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///veritabani.db'  # SQLite veritabanı dosyası yolu
-app.config['SECRET_KEY'] = 'gizli_anahtar'  # Flask-Admin için gizli anahtar
-db = SQLAlchemy(app)
-
-# Veritabanı modeli oluşturma
-class Tablo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    veri = db.Column(db.String(100))
-
-# Flask-Admin yönetici paneli oluşturma
-admin = Admin(app, name='Flask-Admin', template_mode='bootstrap3')
-admin.add_view(ModelView(Tablo, db.session))
-
-if __name__ == '__main__':
-    app.run(debug=True)
-'''
+import sqlite3
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/bedirhankurt/Desktop/Programming/Todist/Development/Database/CardInfo.sqlite'
+db = SQLAlchemy(app)
 
+class GenerellCardInfo(db.Model):
+    __tablename__ = 'Generell Card Info'  # Tablo adını belirtiyoruz, boşluklar için çift tırnak kullanmamız gerekecek
+    id = db.Column(db.Integer, primary_key=True)  # İsteğe bağlı: id sütunu oluşturmak için birincil anahtar ekleyebiliriz
+    Card_Name = db.Column(db.Text)
+    Tags = db.Column(db.Text)
+    Finish_Date = db.Column(db.Text)
+    Card_Description = db.Column(db.Text)
+    Situation = db.Column(db.Text)
+    CardID = db.Column(db.Integer)
+    Number_of_Task = db.Column(db.Integer)
+    Number_of_Completed_Task = db.Column(db.Integer)
+    Number_of_Not_Completed_Task = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<GenerellCardInfo Card_Name="{self.Card_Name}" Tags="{self.Tags}" Finish_Date="{self.Finish_Date}">'
+    
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -31,6 +32,9 @@ def index():
 def workspace():
     return render_template('workspace.html')
 
+@app.route('/logIn')
+def logIn():
+    return render_template('logIn.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
